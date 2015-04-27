c8e167598c65f92430b00aa9e0a4b3b5777f0328
Torky Almohaisen <torky.almohaisen@hotmail.com>
2015-04-26 23:17:16 -0700

    Change implementation of coloring sidebar items

--- functions.php
+++ functions.php
@@ -106,8 +106,6 @@ function underscores_theme_scripts() {
 
 	wp_enqueue_script( 'underscores_theme-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
-	wp_enqueue_script( 'underscores_theme-navColor', get_template_directory_uri() . '/js/navColor.js', array(), '1', true );
-
 	wp_enqueue_script( 'underscores_theme-nav-elements-sizer', get_template_directory_uri() . '/js/nav-elements-sizer.js', array(), '1', true );
 
 
